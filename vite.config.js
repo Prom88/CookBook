a1e2448f92bd2b362ccb5b5448eec32804815e9f
Prom88 <endersteve607@gmail.com>
2024-02-25 01:16:23 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CookBook',
   plugins: [react()],
-  base: '/CookBook/',
+    build: {
+      outDir: 'build', // имя общей выходной директории
+    },
 })
